70f0cd859aa587135594b439bbf332c0bd1edd7d
Rayane Guemmoud <guemmoudrayane03@gmail.com>
2026-03-16 14:46:48 +0100

    feat: add event_exclusions table and backfill user name columns; update PostgreSQL port mapping in docker-compose
    Signed-off-by: Rayane Guemmoud <guemmoudrayane03@gmail.com>

--- database/migrations/002_add_exclusions_table.sql
+++ database/migrations/002_add_exclusions_table.sql
@@ -1,6 +1,6 @@
 -- Add the event_exclusions table to store nominative exclusions for events.
 -- This table will be used to prevent a user from being assigned to another user during the draw.
-CREATE TABLE event_exclusions (
+CREATE TABLE IF NOT EXISTS event_exclusions (
     id SERIAL PRIMARY KEY,
     event_id UUID NOT NULL REFERENCES events(id) ON DELETE CASCADE,
     giver_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
